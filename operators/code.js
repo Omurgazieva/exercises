acca1c8bdb0aca0b299a3ad89b7fccb009176c0e
omurgazieva <omurgazievaasya@gmail.com>
2023-03-02 14:47:04 +0600

    variables

---
+++ operators/code.js
@@ -0,0 +1,20 @@
+// Declare a variable "a" and set it to the number 1
+let a = 1;
+//Declare a variable "b" and set it to the number 2.2
+let b = 2.2;
+//Declare a variable "c" and set it to the number 5000
+let c = 5000;
+//Declare a variable "d" and set it to the number 900000
+let d = 900000;
+//Declare a variable "e" and set it to the number 111222333444
+let e = 111222333444;
+//Declare a variable "isHavingFun" and set it to the boolean true
+let isHavingFun = true;
+//Declare a variable "isDifficult" and set it to the boolean false
+let isDifficult = false;
+//Declare a variable "word" and set it to the string "banana"
+let strBanana = 'word';
+//Declare a variable "sentence" and set it to the string "I ate breakfast."
+var sentence = 'I ate breakfast';
+//Declare a variable "sentence" and set it to the string "The code
+var sentence = "The code in the computer is so hard to understand. It is like magic. If I keep studying maybe one day I'll know";
